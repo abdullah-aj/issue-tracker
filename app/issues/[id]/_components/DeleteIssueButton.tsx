abf00d4ffc962fe72c6802cae4a0229d3927a4cc
abdullah <abdullah.exe@live.com>
2024-11-28 20:21:34 +1300

    show confirmation dialog

--- app/issues/[id]/_components/DeleteIssueButton.tsx
+++ app/issues/[id]/_components/DeleteIssueButton.tsx
@@ -1,4 +1,6 @@
-import { Button, Link } from '@radix-ui/themes'
+'use client'
+
+import { AlertDialog, Button, Flex, Link } from '@radix-ui/themes'
 import { FaRegTrashAlt } from 'react-icons/fa'
 
 type Props = {
@@ -7,12 +9,36 @@ type Props = {
 
 const DeleteIssueButton = ({ issueId }: Props) => {
   return (
-    <Button asChild={true}>
-      <Link color="red" href={`/issues/${issueId}/delete`}>
-        <FaRegTrashAlt />
-        Delete Issue
-      </Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <FaRegTrashAlt />
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deleting?</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This action cannot be undone.
+        </AlertDialog.Description>
+        <Flex mt={'4'} gap={'3'} justify={'end'}>
+          <AlertDialog.Cancel>
+            <Button color="gray" variant="soft">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Confirm</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+    // <Button asChild={true}>
+    //   <Link color="red" href={`/issues/${issueId}/delete`}>
+    //     <FaRegTrashAlt />
+    //     Delete Issue
+    //   </Link>
+    // </Button>
   )
 }
 
