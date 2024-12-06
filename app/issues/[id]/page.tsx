4d8e51c375f2a6c3aabc3ff09800ef9d687ccd0e
abdullah <abdullah.exe@live.com>
2024-12-06 16:03:17 +1300

    optimize performance

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -9,18 +9,23 @@ import IssueDetails from './_components/IssueDetails'
 
 import AuthOptions from '@/app/auth/AuthOptions'
 import prisma from '@/prisma/client'
+import { cache } from 'react'
 
 type Props = {
   params: { id: string }
 }
 
+const fetchIssue = cache((issueId: number) =>
+  prisma.issue.findUnique({
+    where: { id: issueId }
+  })
+)
+
 const IssueDetailPage = async ({ params }: Props) => {
   const session = await getServerSession(AuthOptions)
 
   try {
-    const issue = await prisma.issue.findUnique({
-      where: { id: parseInt(params.id) }
-    })
+    const issue = await fetchIssue(parseInt(params.id))
 
     if (!issue) {
       notFound()
@@ -50,11 +55,7 @@ export default IssueDetailPage
 
 export async function generateMetadata({ params }: Props) {
   try {
-    const issue = await prisma.issue.findUnique({
-      where: {
-        id: parseInt(params.id)
-      }
-    })
+    const issue = await fetchIssue(parseInt(params.id))
 
     return {
       title: issue?.title,
--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable}`}>
+      <body className={`${inter.variable}`} suppressHydrationWarning={true}>
         <AuthProvider>
           <QueryClientProvider>
             <Theme accentColor="blue" radius="small">
